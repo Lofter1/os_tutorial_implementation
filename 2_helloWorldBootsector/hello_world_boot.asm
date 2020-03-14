100ea70711394e093ef1a4c857c359548e958f0a
Justin Juers <justin099@web.de>
2020-03-15 00:03:20 +0100

    Added better comments

--- 2_helloWorldBootsector/hello_world_boot.asm
+++ 2_helloWorldBootsector/hello_world_boot.asm
@@ -1,4 +1,4 @@
-mov ah, 0x0e ; set tty mode
+mov ah, 0x0e ; set tty mode BIOS routine
 mov al, 'H'
 int 0x10
 mov al, 'e'
@@ -11,5 +11,5 @@ int 0x10
 
 jmp $
 
-times 510-($-$$) db 0
-dw 0xaa55
+times 510-($-$$) db 0 ; Padding between instructions and magic number
+dw 0xaa55 ; The magic number indicating this is boot instructions
